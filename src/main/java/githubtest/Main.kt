d54de6818318f722340cdf23950da7c6302c44b1
uknow-mac <yoon6763@naver.com>
2025-01-28 16:55:00 +0900

    레포지토리 가져오기 페이지네이션 정의

--- src/main/java/githubtest/Main.kt
+++ src/main/java/githubtest/Main.kt
@@ -5,14 +5,48 @@ import okhttp3.Request
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import githubtest.dto.commit.CommitResponse
+import githubtest.dto.repository.Repository
 
-const val token = "github_personal_access_token"
+const val token = "github personal access token"
 
 fun main() {
     val owner = "yoon6763"
-    val repo = "objects-practice"
 
-    fetchGitHubCommits(owner, repo, token)
+    val githubRepositories = getGithubRepositoryList(owner)
+
+    for (repository in githubRepositories) {
+        println("repository: ${repository.name}")
+    }
+}
+
+fun getGithubRepositoryList(owner: String): List<Repository> {
+    val client = OkHttpClient()
+    val objectMapper = jacksonObjectMapper()
+
+    val repositories = ArrayList<Repository>()
+    var page = 1
+    val perPage = 100 // 최대 100개까지 가져올 수 있음
+
+    while (true) {
+        val url = "https://api.github.com/users/$owner/repos?per_page=$perPage&page=$page"
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Authorization", "Bearer $token")
+            .build()
+
+        val response = client.newCall(request).execute()
+        val body = response.body?.string()
+
+        if (!response.isSuccessful || body.isNullOrEmpty()) break
+
+        val pageRepositories: List<Repository> = objectMapper.readValue(body)
+        if (pageRepositories.isEmpty()) break // 더 이상 가져올 데이터가 없을 경우 종료
+
+        repositories.addAll(pageRepositories)
+        page++
+    }
+
+    return repositories
 }
 
 fun fetchGitHubCommits(owner: String, repo: String, token: String) {
